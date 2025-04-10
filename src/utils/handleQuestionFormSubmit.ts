e427889e0d54fc32c5f55412ef3754ddb249a9a7
Christopher Tonnesland <christopher.tonnesland@redgo.no>
2025-04-10 12:32:30 +0200

    Adding versel.json

--- src/utils/handleQuestionFormSubmit.ts
+++ src/utils/handleQuestionFormSubmit.ts
@@ -47,7 +47,7 @@ export const handleQuestionFormSubmit = async (values: FormValues, { setSubmitti
     const recipients = RECIPIENT_EMAILS;
 
     // Send the form data to your Vercel function
-    const response = await axios.post("/email/route.ts", {
+    const response = await axios.post("/api/email/route", {
       htmlContent,
       subject,
       recipients,
